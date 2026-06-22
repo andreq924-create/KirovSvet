8b6d7f0a9b78b11d4636cfe7a47b562673e209a4
andreq924-create <andreq924@gmail.com>
2026-06-22 22:06:51 +0300

    Add files via upload

--- api/getRequests.js
+++ api/getRequests.js
@@ -67,7 +67,9 @@ export default async function handler(req, res) {
       );
     }
 
-    return res.status(200).json(result);
+    return res.status(200).json({
+    requests: result
+});
 
   } catch (err) {
     console.error('SERVER ERROR:', err);
